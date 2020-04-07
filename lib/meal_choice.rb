ce7204f201ff995ef7d8006d89c3f66d157a7ef2
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-07 17:50:50 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,5 @@ def meal_choice(veg1,veg2,protein="tofu")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
-meal_choice('carrots', 'string beans', 'lentils')
+meal_choice("broccoli", "macaroni"
 # For output purposes, use "puts" instead of "print" or "p"
